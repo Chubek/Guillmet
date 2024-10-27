ae31dfc8e478e744b6cdf3bf2ab832c1db2842d0
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-10-27 13:46:19 +0330

    Added, and finished for now: The `Stream` module to be used with scanner, parser (and possibly other uses in the future?)

---
+++ Stream.ml
@@ -0,0 +1,78 @@
+module Stream = struct
+  type t = 'a Seq.t ref
+
+  exception Empty_stream
+  exception Consume_failed
+
+  let empty = ref Seq.empty
+
+  let is_spent stm = Seq.is_empty !stm
+
+  let dup stm =
+    let new_stream = empty in
+    Seq.iter (fun i -> new_stream <- i) !stm;
+    new_stream
+
+  let peek stm =
+    match !stm () with
+    | Seq.Nil -> raise Empty_stream
+    | Seq.Cons (hd, _) -> hd
+
+  let next stm =
+    match !stm () with
+    | Seq.Nil -> raise Empty_stream
+    | Seq.Cons (hd, tl) ->
+      stm := tl; hd
+
+  let npeek stm n =
+    let stm' = dup stm in
+    let rec aux acc n' =
+      if n = 0 then List.rev acc
+      else ((next stm') :: acc) (decr n)
+
+  let peek_opt stm =
+    match !stm () with
+    | Seq.Nil -> None
+    | Seq.Cons (hd, _) -> Some hd
+
+  let skip_next stm =
+    let _ = next stm in
+    stm
+
+  let remaining stm = Seq.length !stm
+
+  let append stm lst = 
+    stm := Seq.append !stm (List.to_seq lst)
+
+  let (<<-) i stm = 
+    append stm [i]
+
+  let (>>) stm =
+    skip_next stm
+
+  let take_while pred stm =
+    let rec aux acc stm' =
+      match peek_opt stm' with
+      | Some i when pred i -> aux (i :: acc) (>> stm')
+      | Some _ -> List.rev acc
+      | _ -> raise Consume_failed
+    in
+    aux [] stm
+
+  let take_until pred stm =
+    let rec aux acc stm' =
+      match peek_opt stm' with
+      | Some i when pred i -> List.rev acc
+      | Some i -> aux (i :: acc) (>> stm')
+      | _ -> raise Consume_failed
+    in
+    aux [] stm
+
+  let drop_while pred stm =
+    let _ = take_while pred stm in
+    ()
+
+  let drop_until pred stm =
+    let _ = take_until pred stm in
+    ()
+end
